f6f28a20e5b1c65cd07cb4e31ceaff46ba8e9f35
Oskar Przybysz <wellnessinstructor.oskar@gmail.com>
2023-05-09 11:13:29 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const setColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.body;
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  setColor.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+  console.log(randomColor);
+});
