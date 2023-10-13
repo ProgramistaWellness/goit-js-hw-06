b1918ad547a91b08daa6f7e094bc6c8511b12213
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-10-13 13:23:49 +0200

    Update task-07.js
    reset task

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,7 +1,1 @@
-const input = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
 
-input.addEventListener(
-  "input",
-  () => (text.style.fontSize = input.value + "px")
-);
