4cce20d716a2e6c9503add5a9ec4dcf797c62045
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-10-13 13:24:18 +0200

    Update task-09.js
    reset task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,16 +1,1 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
-const setColor = document.querySelector(".color");
-const changeColorBtn = document.querySelector(".change-color");
-const body = document.body;
-
-changeColorBtn.addEventListener("click", () => {
-  const randomColor = getRandomHexColor();
-  setColor.textContent = randomColor;
-  body.style.backgroundColor = randomColor;
-  console.log(randomColor);
-});
