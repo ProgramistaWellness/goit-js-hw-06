a22c483265170feecf78c331f8cb5f7942e44692
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-10-18 11:03:20 +0200

    finished task-07

---
+++ src/js/task-07.js
@@ -0,0 +1,12 @@
+//Napisz skrypt, który reaguje na zmianę wartości elementu input#font-size-control 
+//(zdarzenie input) i zmienia styl inline span#text aktualizując właściwość font-size.
+// W rezultacie, podczas zmiany wartości suwakiem, ma zmieniać się rozmiar tekstu w elemencie span
+
+const inputControl = document.querySelector("#font-size-control");
+const spanText = document.querySelector("#text");
+const changeFontSize = () => {
+ const currentSize = parseInt(inputControl.value);
+ spanText.style.fontSize = `${currentSize}px`;
+};
+
+inputControl.addEventListener("input", changeFontSize);
